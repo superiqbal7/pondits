8ecf5128a3632950bdedad2098ebceb5650d68cc
Rahim Iqbal <superiqu@gmail.com>
2022-10-16 23:09:34 +0600

    map task

---
+++ map.js
@@ -0,0 +1,9 @@
+const users = [
+  { firstName: 'John', lastName: 'Doe', age: 21},
+  { firstName: 'Rahim', lastName: 'Iqbal', age: 26},
+  { firstName: 'Shakib', lastName: 'Al Hasan', age: 33},
+  { firstName: 'Lionel', lastName: 'Messi', age: 34},
+  { firstName: 'Christiano', lastName: 'Ronaldo', age: 37}
+];
+
+// Create an array that contains full name of each person who are older than 30
